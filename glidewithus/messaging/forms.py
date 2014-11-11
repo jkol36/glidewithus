732a9478c3ebb4678535a89596e4375fd4821f0b
= <=jonathankolman@gmail.com>
2014-11-11 00:10:01 +0000

    fuckibitchesgetpaid

--- glidewithus/messaging/forms.py
+++ glidewithus/messaging/forms.py
@@ -3,7 +3,7 @@ from glidewithus.profiles.models import GlideProfile
 from models import message
 
 class new_message(forms.ModelForm):
-	text = forms.CharField(label="", widget=forms.TextInput(attrs={"id":'message_text', 'class':'form-control', 'rows':5 }))
+	text = forms.CharField(label="", widget=forms.Textarea(attrs={"id":'message_text', 'class':'form-control', 'rows':5, 'placeholder':'Your message here' }))
 
 	class Meta:
 		model = message
